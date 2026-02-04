3cf5ed42f1edbcec782dd91bc50e8756c57b3bf5
bArA0033 <bhradmhranpwr@gmail.com>
2026-02-04 11:22:07 -0800

    adding employee model
    now we have date of employment and job position just for employees and not for the passengers.

--- airport/models.py
+++ airport/models.py
@@ -6,6 +6,16 @@ def profile_upload_to(instance, filename):
     return f'profile_pics/{instance.username}/{filename}'
 
 class User(AbstractUser):
+    # data
+    phone_number = models.CharField(max_length=11)
+    email = models.EmailField(blank=True)
+    profile_pic = models.ImageField(upload_to=profile_upload_to, blank=True, null=True)
+
+    # dates
+    date_of_birth = models.DateField(blank=True, null=True)
+
+
+class Employee(models.Model):
     # inner classes
     class Positions(models.TextChoices):
         manager = 'ma', 'manager'
@@ -15,15 +25,15 @@ class User(AbstractUser):
         pilot = 'pi', 'pilot'
         airport_employee = 'ae', 'airport_employee'
         passenger = 'pa', 'passenger'
+
+    # foreign relations
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='employee_profile')
+
     # data
     job_position = models.CharField(choices=Positions.choices, default=Positions.passenger, max_length=100)
-    phone_number = models.CharField(max_length=11)
-    email = models.EmailField(blank=True)
-    profile_pic = models.ImageField(upload_to=profile_upload_to, blank=True, null=True)
 
-    # dates
-    date_of_employment = models.DateField(blank=True, null=True)
-    date_of_birth = models.DateField(blank=True, null=True)
+    # date
+    date_of_employment = models.DateField()
 
 
 
