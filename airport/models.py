88971df49e0d2e66a2543c08ffc2956266dd6e38
bArA0033 <bhradmhranpwr@gmail.com>
2026-02-04 10:07:06 -0800

    adding User  model

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+def profile_upload_to(instance, filename):
+    return f'profile_pics/{instance.username}/{filename}'
+
+class User(AbstractUser):
+    # inner classes
+    class Positions(models.TextChoices):
+        manager = 'ma', 'manager'
+        administrator = 'ad', 'administrator'
+        gate_gard = 'gg', 'gate_gard'
+        flight_crew = 'fc', 'flight_crew'
+        pilot = 'pi', 'pilot'
+        airport_employee = 'ae', 'airport_employee'
+        passenger = 'pa', 'passenger'
+    # data
+    job_position = models.CharField(choices=Positions.choices, default=Positions.passenger, max_length=100)
+    phone_number = models.CharField(max_length=11)
+    email = models.EmailField(blank=True)
+    profile_pic = models.ImageField(upload_to=profile_upload_to, blank=True, null=True)
+
+    # dates
+    date_of_employment = models.DateField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+
