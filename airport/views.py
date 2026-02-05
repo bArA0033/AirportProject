bd684b4c00b07d4c3ea4d9a6cb09f30bc5fb04b5
bArA0033 <bhradmhranpwr@gmail.com>
2026-02-05 03:42:53 -0800

    adding home view and profile view
    home view is steel incomplete and profile view is considered to handle profile logic

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from airport.models import Flight
+
+
+def home(request):
+    pass
+
+@login_required
+def profile(request):
+    if request.user.employee:
+        flights = Flight.objects.filter(pilot=request.user.employee)
+        person = request.user.employee
+    else:
+        flights = Flight.objects.filter(passengers=request.user.passenger)
+        person = request.user.passenger
+
+    context = {
+        'flights':flights,
+        'person':person,
+    }
+
+    return render(request,'',context)
